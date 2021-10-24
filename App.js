2be022a1c0955a29f1bde660fa697c4e36e2a523
IK-R-S <73291742+IK-R-S@users.noreply.github.com>
2021-10-24 11:41:48 -0300

    New code -  QRCODE Improve
    Criando gerador de QRCODE

--- App.js
+++ App.js
@@ -1,26 +1,44 @@
 // © EASYCEP - 2021
 
+//import { ToastAndroid } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import addcep from './assets/addcep.png'
+import { useFonts, Quicksand_300Light, Quicksand_400Regular, Quicksand_500Medium, Quicksand_700Bold, } from '@expo-google-fonts/quicksand';
+import { AppLoading } from 'expo-app-loading';
+import QRCode from 'qrcode';
+import addcep from './assets/addcep.png';
 import api from './services/api';
-import styles from './styles/styles'
+import styles from './styles/styles';
 
 export default function App() {
+    // Usando fontes personalizadas
+    let FontsLoaded = useFonts({
+        Quicksand_300Light,
+        Quicksand_400Regular,
+        Quicksand_500Medium,
+        Quicksand_700Bold,
+    })
+
+    if(!FontsLoaded){
+        return <AppLoading/>
+    }
 
     // Definindo dados de consulta e resposta
     const [cep, setCep] = useState('')
     const [result, setResult] = useState(false)
 
-    // Criando a constante de consulta na API
+    // Criando a constante de consulta na API + Execução da Const de ToastAndroid
     const consultarCep = () => {
+        //showToast();
+
         api.get(`/ws/${cep}/json/`).then((response) => {
             setResult(response.data);
         }).catch(() => {
             setResult({ cep });
-        })    
+        })
+            
     }
 
     // Construção de dinâmica de Hyperlink do Maps
@@ -34,13 +52,25 @@ export default function App() {
     function openMaps(){
         openInNewTab(link)
     }
-    
+
+    // Criando o gerador de Qrcodes - EM TESTES
+    const [qr, setQr] = useState('')
+    useEffect(() => {
+        QRCode.toDataURL(link).then(setQr)
+    }, [])
+
+    // Toast de carregamento
+    /*
+    const showToast = () => {
+        ToastAndroid.show('Carregando, aguarde...', ToastAndroid.SHORT)
+    }
+    */
     // Retorno da função no Front End
     return (
         <View style={styles.container}>
             <View style={styles.box}>
                 <View style={styles.searchBox}>
-                    <Ionicons style={styles.iconSearch} name='location-outline' size={20} color="white" />
+                    <Ionicons style={styles.iconSearch} name='location-outline' size={25} color="white" />
                     <TextInput onChangeText={(text) => setCep(text)} style={styles.searchInput} placeholder="Insira um CEP" keyboardType="numeric" />
                 </View>
                 <TouchableOpacity onPress={consultarCep} style={styles.searchButton}>
@@ -51,6 +81,7 @@ export default function App() {
             {
                 result.uf ? (
                     <>
+                        <Image src={qr} style={styles.qrcode} />
                         <Text style={styles.cepTitle}>{result?.cep}</Text>
                         <View style={styles.resultsBox}>
                             <Text style={styles.results}>Bairro: {result?.bairro}</Text>
@@ -89,7 +120,7 @@ export default function App() {
 
                 )
             }
-            <StatusBar style="auto" />
+            <StatusBar hidden={true} />
         </View>
     );
 }
