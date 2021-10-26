471486fb4c8c0b67c63589588b721684dedf12a7
IK-R-S <73291742+IK-R-S@users.noreply.github.com>
2021-10-26 13:52:43 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 // © EASYCEP - 2021
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Route from "./components/routes";
+import Route from "./src/routes";
 
 export default function App() {
     // Retorno da função no Front End
